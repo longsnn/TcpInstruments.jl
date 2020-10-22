995f4656889f564f268bd2737697f0e690c67f12
Morten F. Rasmussen <10264458+mofii@users.noreply.github.com>
2020-10-22 15:46:28 -0700

    Updated the PSU example.

--- examples/PSU_example.jl
+++ examples/PSU_example.jl
@@ -1,34 +1,33 @@
-using GenericInstruments
-GI = GenericInstruments
+using TcpInstruments
+tcpi = TcpInstruments
 ####################################
 # Instantiate obj
-resmgr   = GI.viOpenDefaultRM()
-psu1 = GI.PSU.INSTR(:KeysightE3645A, "GPIB0::5::INSTR")#single ch psu
-psu2 = GI.PSU.INSTR(:AgilentE3646A, "GPIB0::6::INSTR")#dual ch psu
+psu1 = tcpi.initialize(:KeysightE3645A, "10.0.1.156") #single ch psu
+psu2 = tcpi.initialize(:AgilentE3646A,  "10.0.1.157") #dual ch psu
 ####################################
 # USAGE
 ####################################
 # Power supply
-GI.PSU.connect!(resmgr,psu1)
-GI.PSU.connect!(resmgr,psu2)
+tcpi.connect!(psu1)
+tcpi.connect!(psu2)
 # IDN
-GI.PSU.get_idn(psu1)
-GI.PSU.get_idn(psu2)
+tcpi.get_id(psu1)
+tcpi.get_id(psu2)
 # Reset
-GI.PSU.reset_instr(psu1)
-GI.PSU.reset_instr(psu2)
+tcpi.reset_instr(psu1)
+tcpi.reset_instr(psu2)
 # Set range
-GI.PSU.set_range(psu1,ch="1",vrang="low")
-GI.PSU.set_range(psu2,ch="1",vrang="high")
+tcpi.set_range(psu1, ch="1", vrang="low")
+tcpi.set_range(psu2, ch="1", vrang="high")
 # Set voltage
-GI.PSU.set_volt(psu1,ch="1",volt=5)
-GI.PSU.set_volt(psu2,ch="1",volt=3)
+tcpi.set_volt(psu1, ch="1", volt=5)
+tcpi.set_volt(psu2, ch="1", volt=3)
 # Set current compliance
-GI.PSU.set_compl(psu1,ch="1",crtlim=0.6)
-GI.PSU.set_compl(psu2,ch="1",crtlim=1.1)
+tcpi.set_compl(psu1, ch="1", crtlim=0.6)
+tcpi.set_compl(psu2, ch="1", crtlim=1.1)
 # Output on/off
-GI.PSU.set_outp(psu1,ch="1",st="on")
-GI.PSU.set_outp(psu2,ch="1",st="on")
+tcpi.set_outp(psu1, ch="1", st="on")
+tcpi.set_outp(psu2, ch="1", st="on")
 # Disconnect everything
-GI.PSU.disconnect!(psu1)
-GI.PSU.disconnect!(psu2)
+tcpi.disconnect!(psu1)
+tcpi.disconnect!(psu2)
