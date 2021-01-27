cef2af82c6eaec1f7165dec0635adeca891fd91d
Morten F. Rasmussen <10264458+mofii@users.noreply.github.com>
2021-01-26 22:39:54 -0800

    Make TcpInstruments.jl export some of the functions.

--- src/TcpInstruments.jl
+++ src/TcpInstruments.jl
@@ -1,11 +1,31 @@
 module TcpInstruments
 
+export 
+        Instrument,
+        Oscilloscope, 
+        MultiMeter,
+        PowerSupply,
+        WaveformGenerator,
+        GenericInstrument,
+
+        instrument_initialize,
+        instrument_reset,
+        instrument_clear,
+        instrument_get_id,
+        instrument_beep_on,
+        instrument_beep_off,
+        # Scope specific commends
+        scope_get_ch_data
+
+
+
 # common instrument containers
 include("instrument_type_definitions.jl")
+include("common_commands.jl")
 # instruments
-#include("psu/PSU.jl")
-#include("awg/AWG.jl")
-include("scope/SCOPE.jl")
-#include("dmm/DMM.jl")
+include("scope/scope.jl")
+#include("psu/psu.jl")
+#include("awg/awg.jl")
+#include("dmm/dmm.jl")
 
 end #endmodule
