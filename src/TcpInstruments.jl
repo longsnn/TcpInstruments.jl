b221a34bf3b5af1418317c4f81d4673d82b67167
Esti Gajda <egajda@umich.edu>
2021-03-26 17:14:26 -0400

    Exports

--- src/TcpInstruments.jl
+++ src/TcpInstruments.jl
@@ -49,8 +49,8 @@ export Oscilloscope, MultiMeter, PowerSupply, WaveformGenerator, ImpedanceAnalyz
 
 export save, load
 
-export initialize, terminate, reset,
-export remote_mode, local_mode,
+export initialize, terminate, reset
+export remote_mode, local_mode
 export query, write, info, connect!, close!
 
 # Power Supply
@@ -61,16 +61,16 @@ export set_voltage_limit, get_voltage_limit
 export set_channel, get_channel
 
     # Scope
-export get_data,
-export lpf_on, lpf_off, get_lpf_state,
+export get_data
+export lpf_on, lpf_off, get_lpf_state
 export set_impedance_1Mohm, set_impedance_50ohm, get_impedance
 export get_coupling
 export get_function, set_function
-export get_frequency, set_frequency,
-export get_amplitude, set_amplitude,
-export get_voltage_offset, set_voltage_offset,
+export get_frequency, set_frequency
+export get_amplitude, set_amplitude
+export get_voltage_offset, set_voltage_offset
 export set_burst_mode_gated, set_burst_mode_triggered, get_burst_mode
-export get_mode, set_mode_burst, set_mode_cw,
+export get_mode, set_mode_burst, set_mode_cw
 
 
 # Prologix
@@ -79,37 +79,37 @@ export set_prologix_chan, get_prologix_chan, scan_prologix
 
 # DMM
 export get_tc_temperature, set_tc_type
-export get_current,
-export get_resistance,
-export set_temp_unit_celsius, set_temp_unit_farenheit, set_temp_unit_kelvin,
-export get_temp_unit,
+export get_current
+export get_resistance
+export set_temp_unit_celsius, set_temp_unit_farenheit, set_temp_unit_kelvin
+export get_temp_unit
 
 # Impedance
-export get_bandwidth, set_bandwidth,
-export get_frequency_limits, set_frequency_limits,
-export get_num_data_points, set_num_data_points,
-export get_volt_dc, set_volt_dc,
-export get_volt_ac, set_volt_ac,
-export get_volt_limit_dc, set_volt_limit_dc,
+export get_bandwidth, set_bandwidth
+export get_frequency_limits, set_frequency_limits
+export get_num_data_points, set_num_data_points
+export get_volt_dc, set_volt_dc
+export get_volt_ac, set_volt_ac
+export get_volt_limit_dc, set_volt_limit_dc
 
 # Signal Generator
-export get_frequency, set_frequency,
-export get_amplitude, set_amplitude,
-export get_burst_num_cycles, set_burst_num_cycles,
-export get_time_offset, set_time_offset,
-export get_voltage_offset, set_voltage_offset,
-export get_burst_period, set_burst_period,
-export get_mode, set_mode_burst, set_mode_cw,
+export get_frequency, set_frequency
+export get_amplitude, set_amplitude
+export get_burst_num_cycles, set_burst_num_cycles
+export get_time_offset, set_time_offset
+export get_voltage_offset, set_voltage_offset
+export get_burst_period, set_burst_period
+export get_mode, set_mode_burst, set_mode_cw
 
 
 
 # Devices
 ## Impedance Analyzer
-export Agilent4294A, Agilent4395A,
+export Agilent4294A, Agilent4395A
 ## Multimeter
-export KeysightDMM34465A,
+export KeysightDMM34465A
 ## Scope
-export AgilentDSOX4024A, AgilentDSOX4034A,
+export AgilentDSOX4024A, AgilentDSOX4034A
 ## Power Supply
 export AgilentE36312A, SRSPS310, VersatilePower
 ## Waveform Generator
