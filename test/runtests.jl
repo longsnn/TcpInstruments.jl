c4d59aa2c0db51ced7c58c24d68b15e2d8182043
Morten F. Rasmussen <10264458+mofii@users.noreply.github.com>
2021-05-17 22:07:42 -0700

    Created top-level extended testset in runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,34 +8,35 @@ using Unitful
 
 const A = u"A"
 
+@testset ExtendedTestSet "TcpInstruments" begin
+    @testset "Fake Scope" begin
+        f = initialize(TcpInstruments.FakeDSOX4034A)
 
-@testset "Fake Scope" begin
-    f = initialize(TcpInstruments.FakeDSOX4034A)
+        data = get_data(f, 1)
+        @test data isa TcpInstruments.ScopeData
+        @test length(data.time) == length(data.volt)
+        @test data.volt[1] isa Unitful.Voltage
 
-    data = get_data(f, 1)
-    @test data isa TcpInstruments.ScopeData
-    @test length(data.time) == length(data.volt)
-    @test data.volt[1] isa Unitful.Voltage
+        data = get_data(f, [1,2,3,4])
+        @test length(data) == 4
+    end
 
-    data = get_data(f, [1,2,3,4])
-    @test length(data) == 4
-end
+    @testset "TcpInstruments.jl" begin
+        include("./emulate/test_fake_device.jl")
+    end
 
-@testset "TcpInstruments.jl" begin
-    include("./emulate/test_fake_device.jl")
-end
+    @testset "Util Functions" begin
+        @testset "split_str_into_host_and_port" begin
+            host = "192.168.1.1"
+            port = 5055
+            host_str = "$host:$port"
+            host_out, port_out = TcpInstruments.split_str_into_host_and_port(host_str)
+            @test host_out == host
+            @test port_out == port
 
-@testset "Util Functions" begin
-    @testset "split_str_into_host_and_port" begin
-        host = "192.168.1.1"
-        port = 5055
-        host_str = "$host:$port"
-        host_out, port_out = TcpInstruments.split_str_into_host_and_port(host_str)
-        @test host_out == host
-        @test port_out == port
-
-        host_out, port_out = TcpInstruments.split_str_into_host_and_port(host)
-        @test host_out == host
-        @test port_out == 0
+            host_out, port_out = TcpInstruments.split_str_into_host_and_port(host)
+            @test host_out == host
+            @test port_out == 0
+        end
     end
 end
