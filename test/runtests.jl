247d74c4b6a2e226284ef9caae2e6136aa978d60
Morten F. Rasmussen <10264458+mofii@users.noreply.github.com>
2021-12-08 23:39:37 -0800

    Updated unit tests from format_time() to get_nearest_scale_and_time_unit()

--- test/runtests.jl
+++ test/runtests.jl
@@ -10,12 +10,16 @@ const A = u"A"
 
 @testset ExtendedTestSet "TcpInstruments" begin
 
-    "Helper function: expected_number_and_unit(function_name, val, true_val_scaled, true_unit)"
-    function expected_number_and_unit(function_name, val, true_val_scaled, true_unit)
-        scaled_val, unit = function_name(val)
+    function expected_number_and_unit(function_name, val, true_factor, true_val_scaled, true_unit)
+        scaled_val, factor, unit = function_name(val)
+
         same = (scaled_val ≈ true_val_scaled)
-        if same == true
-            same = (unit == true_unit)
+        same && (same = unit == true_unit)
+        same && (same = factor == true_factor)
+
+        if !same
+            @info "true"   true_val_scaled, true_factor, true_unit
+            @info "scaled"      scaled_val,      factor,      unit
         end
 
         return same
@@ -68,39 +72,42 @@ const A = u"A"
             rm(filename*".mat")
         end
 
-        @testset "format_time" begin
-            using TcpInstruments: format_time
-            @test expected_number_and_unit(format_time, 1e3,     1000,   "s")
-            @test expected_number_and_unit(format_time, 1,          1,   "s")
-            @test expected_number_and_unit(format_time, 0,          0,   "s")
-            @test expected_number_and_unit(format_time, -1,        -1,   "s")
-            @test expected_number_and_unit(format_time, -2e-2,    -20,  "ms")
-            @test expected_number_and_unit(format_time, -3e-3,     -3,  "ms")
-            @test expected_number_and_unit(format_time, -4e-4,   -400,  "µs")
-            @test expected_number_and_unit(format_time, -5e-5,    -50,  "µs")
-            @test expected_number_and_unit(format_time, -6e-6,     -6,  "µs")
-            @test expected_number_and_unit(format_time, -7e-7,   -700,  "ns")
-            @test expected_number_and_unit(format_time, -8e-8,    -80,  "ns")
-            @test expected_number_and_unit(format_time, -9e-9,     -9,  "ns")
-            @test expected_number_and_unit(format_time, -10e-10,   -1,  "ns")
-            @test expected_number_and_unit(format_time, -11e-11, -110,  "ps")
-            @test expected_number_and_unit(format_time, -12e-12,  -12,  "ps")
-            @test expected_number_and_unit(format_time, -13e-13, -1.3,  "ps")
-            @test expected_number_and_unit(format_time, -14e-14, -0.14, "ps")
+        @testset "get_nearest_scale_and_time_unit" begin
+            using TcpInstruments: get_nearest_scale_and_time_unit
+            @test expected_number_and_unit(get_nearest_scale_and_time_unit, 1e3,     1, 1000,   "s")
+            @test expected_number_and_unit(get_nearest_scale_and_time_unit, 1,       1,    1,   "s")
+            @test expected_number_and_unit(get_nearest_scale_and_time_unit, 0,       1,    0,   "s")
+            @test expected_number_and_unit(get_nearest_scale_and_time_unit, -1,      1,   -1,   "s")
+            @test expected_number_and_unit(get_nearest_scale_and_time_unit, -2e-2,   1e3,  -20,  "ms")
+            @test expected_number_and_unit(get_nearest_scale_and_time_unit, -3e-3,   1e3,   -3,  "ms")
+            @test expected_number_and_unit(get_nearest_scale_and_time_unit, -4e-4,   1e6, -400,  "µs")
+            @test expected_number_and_unit(get_nearest_scale_and_time_unit, -5e-5,   1e6,  -50,  "µs")
+            @test expected_number_and_unit(get_nearest_scale_and_time_unit, -6e-6,   1e6,   -6,  "µs")
+            @test expected_number_and_unit(get_nearest_scale_and_time_unit, -7e-7,   1e9, -700,  "ns")
+            @test expected_number_and_unit(get_nearest_scale_and_time_unit, -8e-8,   1e9,  -80,  "ns")
+            @test expected_number_and_unit(get_nearest_scale_and_time_unit, -9e-9,   1e9,   -9,  "ns")
+            @test expected_number_and_unit(get_nearest_scale_and_time_unit, -10e-10, 1e9,   -1,  "ns")
+            @test expected_number_and_unit(get_nearest_scale_and_time_unit, -11e-11, 1e12, -110,  "ps")
+            @test expected_number_and_unit(get_nearest_scale_and_time_unit, -12e-12, 1e12,  -12,  "ps")
+            @test expected_number_and_unit(get_nearest_scale_and_time_unit, -13e-13, 1e12, -1.3,  "ps")
+            @test expected_number_and_unit(get_nearest_scale_and_time_unit, -14e-14, 1e12, -0.14, "ps")
+
         end
 
 
-        @testset "format_volt" begin
-            using TcpInstruments: format_volt
-            @test expected_number_and_unit(format_volt, 1001, 1.001, "kV")
-            @test expected_number_and_unit(format_volt, 1000,  1000,  "V")
-            @test expected_number_and_unit(format_volt, 1.1,    1.1,  "V")
-            @test expected_number_and_unit(format_volt, 0,        0,  "V")
-            @test expected_number_and_unit(format_volt, 1e-1,   100, "mV")
-            @test expected_number_and_unit(format_volt, 2.2e-2,  22, "mV")
-            @test expected_number_and_unit(format_volt, 3.3e-3, 3.3, "mV")
-            @test expected_number_and_unit(format_volt, 3.3e-6, 3.3, "µV")
-            @test expected_number_and_unit(format_volt, 7.6e-9, 7.6, "nV")
+        @testset "get_nearest_scale_and_volt_unit" begin
+            using TcpInstruments: get_nearest_scale_and_volt_unit
+            @test expected_number_and_unit(get_nearest_scale_and_volt_unit, 1.1e9, 1e-9, 1.1, "GV")
+            @test expected_number_and_unit(get_nearest_scale_and_volt_unit, 1.2e6, 1e-6, 1.2, "MV")
+            @test expected_number_and_unit(get_nearest_scale_and_volt_unit, 1001,  1e-3, 1.001, "kV")
+            @test expected_number_and_unit(get_nearest_scale_and_volt_unit, 1000,     1, 1000,  "V")
+            @test expected_number_and_unit(get_nearest_scale_and_volt_unit, 1.1,      1,  1.1,  "V")
+            @test expected_number_and_unit(get_nearest_scale_and_volt_unit, 0,        1,    0,  "V")
+            @test expected_number_and_unit(get_nearest_scale_and_volt_unit, 1e-1,   1e3,  100, "mV")
+            @test expected_number_and_unit(get_nearest_scale_and_volt_unit, 2.2e-2, 1e3,   22, "mV")
+            @test expected_number_and_unit(get_nearest_scale_and_volt_unit, 3.3e-3, 1e3,   3.3, "mV")
+            @test expected_number_and_unit(get_nearest_scale_and_volt_unit, 3.3e-6, 1e6,   3.3, "µV")
+            @test expected_number_and_unit(get_nearest_scale_and_volt_unit, 7.6e-9, 1e9,   7.6, "nV")
         end
 
 
