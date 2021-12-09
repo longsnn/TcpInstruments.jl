f4890b76e19c88cbbff6f8fb57a2264ab180d8d5
Morten F. Rasmussen <10264458+mofii@users.noreply.github.com>
2021-12-09 01:02:31 -0800

    Refactoring of the fake device

--- test/emulate/fake_device.jl
+++ test/emulate/fake_device.jl
@@ -10,36 +10,36 @@ server = listen(port)
 #tcp(msg) = printstyled("[ TCP: " * msg * "\n", color = :blue, bold = true)
 tcp(msg) = []
 
-kill = false
-function close_emulator()
-    global kill
-    kill = true
-end
+const CONNECTION_OPEN = [true]
+get_connection_open() = CONNECTION_OPEN[1]
+set_connection_open(state::Bool) = CONNECTION_OPEN[1] = state
+connection_is_open() = get_connection_open()
+connection_is_closed() = !get_connection_open()
+close_emulator() = set_connection_open(false)
 
 function emulator()
     while true
-        global kill
         conn = accept(server)
         @async begin
-          try
-         while true
-                if kill
-                    exit()
-                end
-                line = readline(conn)
-                if !isempty(line)
-                    if line == "*IDN?"
-                        write(conn, "1\n")
-                        tcp("1")
-                    else
-                        tcp(line)
+            try
+                while true
+                    if connection_is_closed()
+                        exit()
                     end
+                    line = readline(conn)
+                    if !isempty(line)
+                        if line == "*IDN?"
+                            write(conn, "1\n")
+                            tcp("1")
+                        else
+                            tcp(line)
+                        end
+                    end
+                    sleep(0.0001)
                 end
-                sleep(0.0001)
+            catch err
+                print("connection ended with error $err")
             end
-          catch err
-            print("connection ended with error $err")
-          end
         end
     end
 end
