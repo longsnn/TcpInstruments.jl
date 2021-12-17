fcbfcb2fa5ed575785870e7bd51769007c05f91f
Steve Leung <steve@orchardultrasound.com>
2021-12-16 17:48:16 -0800

    Reorganized docs page hierarchy

--- docs/make.jl
+++ docs/make.jl
@@ -8,10 +8,12 @@ makedocs(
         prettyurls = get(ENV, "CI", nothing) == "true"
     ),
     pages = [
-             "Home" => "index.md",
-             "Supported Instruments" => "instruments.md",
-             "Functions" => "functions.md",
-            ],
+        "Home" => "index.md",
+        "Manual" => Any[
+            "Supported Instruments" => "instruments.md",
+            "Instrument-specific Functions" => "functions.md",
+        ]
+    ],
     modules = [TcpInstruments]
 )
 
