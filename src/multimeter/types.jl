1f5a96dad3d1b0956fc50206dd62a89b8d9eea22
Steve Leung <steve@orchardultrasound.com>
2022-03-24 17:45:47 -0700

    Updated docstring for KeysightMultimeter

--- src/multimeter/types.jl
+++ src/multimeter/types.jl
@@ -4,14 +4,24 @@
 abstract type MultiMeter <: Instrument end
 
 """
-# Available functions
-- `initialize`
-- `terminate`
-- `get_tc_temperature` (tc = thermocouple)
-- `get_voltage`
-- `get_current`
-- `get_resistance(;wire)` # wire must be set to 2 or 4
-- `get_channel` # (some kind of input detection not selection)
+Supported models
+- `KeysightDMM34465A`
+
+Supported functions
+- [`initialize`](@ref)
+- [`terminate`](@ref)
+
+
+- [`get_channel`](@ref)
+- [`get_voltage`](@ref)
+- [`get_current`](@ref)
+- [`get_resistance`](@ref)
+- [`get_tc_temperature`](@ref)
+- [`set_tc_type`](@ref)
+- [`get_temp_unit`](@ref)
+- [`set_temp_unit_celsius`](@ref)
+- [`set_temp_unit_farenheit`](@ref)
+- [`set_temp_unit_kelvin`](@ref)
 """
 abstract type KeysightMultimeter <: MultiMeter end
 struct KeysightDMM34465A <: KeysightMultimeter end
